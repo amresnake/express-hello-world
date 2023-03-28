8e1a07b79af8bcccbd4d4434bcca56e8c3abee7a
Amre MAARIR <amre.maarir@emea.adecco.net>
2023-03-28 15:30:03 +0000

    fix public paths

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+var path = require('path');
+
+var public = path.join(__dirname, '/html-template');
+app.use(express.static(public));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/html-template/index.html"));
 
