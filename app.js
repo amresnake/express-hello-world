6539f03431aee9d482bba94f931cae66ccca1f14
Amre MAARIR <amre.maarir@emea.adecco.net>
2023-03-28 15:23:39 +0000

    fix template path

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.sendFile(__dirname + "html-template/index.html"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/html-template/index.html"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
